dbdcfa5a91a22bac615f155987d43943b3fb9d7f
Matt Hackmann <matt.hackmann@gmail.com>
2023-06-06 21:34:40 -0700

    polish for stats page

--- app/src/pages/stats/index.tsx
+++ app/src/pages/stats/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { useDevice } from '@app/hooks/useDevice';
 import { useStats } from '@app/hooks/useStats';
-import { Box, Container, Heading, Spinner, Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react';
+import { Box, Card, CardBody, CardHeader, Container, Heading, Spinner, Stack, Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react';
+import EventIcon from '@app/components/EventIcon';
+import { IconSize } from '@app/components/EventIcon';
+import { useButtons } from '@app/hooks/useButtons';
 
 function formatDuration(duration: number) {
   let durationInSeconds = duration / 1000;
@@ -23,18 +26,30 @@ function formatDuration(duration: number) {
 
 const StatsPage = (): React.ReactElement => {
   const deviceId = useDevice();
-  const { isLoading, hasError, rollupStats } = useStats(deviceId);
+  const { isLoading: isStatsLoading, hasError: hasStatsError, rollupStats } = useStats(deviceId);
+  const { isLoading: isButtonsLoading, hasError: hasButtonsError, buttonLabels } = useButtons(deviceId);
+  const isLoading = useMemo(() => isStatsLoading || isButtonsLoading, [ isStatsLoading, isButtonsLoading ]);
+  const hasError = useMemo(() => hasStatsError || hasButtonsError, [ hasStatsError, hasButtonsError ]);
 
   return (
     <Container>
       <Heading>Stats</Heading>
       {isLoading && <Spinner color="purple.500" size="xl" />}
       {!isLoading && !hasError && rollupStats.map(stat => (
-        <Stat>
-          <StatLabel>{stat.eventDataType}</StatLabel>
-          <StatNumber>{stat.count}</StatNumber>
-          {stat.duration && <StatHelpText>{formatDuration(stat.duration)}</StatHelpText>}
-        </Stat>
+        <Card direction="row" mb={3} alignItems="center">
+          <CardHeader>
+            <EventIcon eventDataType={stat.eventDataType} size={IconSize.Large} />
+          </CardHeader>
+          <Stack>
+            <CardBody>
+              <Stat>
+                <StatLabel>{buttonLabels[stat.eventDataType]}</StatLabel>
+                <StatNumber>{stat.count}</StatNumber>
+                {stat.duration && <StatHelpText>{formatDuration(stat.duration)}</StatHelpText>}
+              </Stat>
+            </CardBody>
+          </Stack>
+        </Card>
       ))}
     </Container>
   );
